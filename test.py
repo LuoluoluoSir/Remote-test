535f0b2d2c07f8a0d30106afc1b70ef13ae22703
Luo <286275850@qq.com>
2023-09-03 16:54:01 +0800

    再绘制一条余弦曲线

--- test.py
+++ test.py
@@ -5,5 +5,8 @@ import matplotlib.pyplot as plt
 x = np.linspace(0, 4*np.pi, 100)
 y = np.sin(x)
 
+y1 = np.cos(x)
+
 plt.scatter(x, y ,c='black', marker='.')
+plt.scatter(x, y1 ,c='red', marker='.')
 plt.show()
