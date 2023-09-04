d11e82aa0c368d84e13fc8c5365bcb0f1ac4821f
罗Sir <286275850@qq.com>
2023-09-04 20:37:12 +0800

    123

--- test.py
+++ test.py
@@ -3,7 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(0, 4*np.pi, 100)
-y = np.sin(x)
+y = np.sin(x+np.pi)
+
+y1 = np.cos(x)
 
 plt.scatter(x, y ,c='black', marker='.')
+plt.scatter(x, y1 ,c='red', marker='.')
 plt.show()
+
+# aaaa
